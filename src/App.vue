cebc298934f997cfedb6e14c0cf908a1b52280ec
unknown <omiddellcash@gmail.com>
2022-04-19 06:12:18 +0430

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+	<h1>پرسش و پاسخ</h1>
+</template>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+body {
+	background-color: #271c36;
+	color: #fff;
+}
+</style>
